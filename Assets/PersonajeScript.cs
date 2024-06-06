00ce3bc5cda08664f5f0fa5a0383334716746135
EiIoGo <111787747+EiIoGo@users.noreply.github.com>
2024-06-06 09:07:13 -0300

    todo

---
+++ Assets/PersonajeScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersonajeScript : MonoBehaviour
+{
+    public AlimentoScript script;
+
+    public int nivelHambre;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void TomarAlimento(int valorAlimentario)
+    {
+        nivelHambre -= valorAlimentario;
+        if (nivelHambre < 0)
+        {
+            nivelHambre = 0;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("trigger" + other.name);
+        AlimentoScript alimento = other.GetComponent<AlimentoScript>();
+        TomarAlimento(alimento.valorAlimentario);
+    }
+}
